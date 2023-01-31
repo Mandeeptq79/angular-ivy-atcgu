11c3ffd64796940bce417c43c6e756bf6bfd6fd0
Mandeeptq79 <120645256+Mandeeptq79@users.noreply.github.com>
2023-01-31 12:04:41 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-
+import { WarningAltertComponent } from './warning-alert/warning.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [
@@ -14,6 +15,8 @@ import { ServersComponent } from './servers/servers.component';
     HelloComponent,
     ServerComponent,
     ServersComponent,
+    WarningAltertComponent,
+    SuccessAlertComponent,
   ],
   bootstrap: [AppComponent],
 })
